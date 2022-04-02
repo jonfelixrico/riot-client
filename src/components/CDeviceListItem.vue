78ea75855bf3edc697e484e50ef192306665bbc5
Felix <jonfelixrico@gmail.com>
2022-04-02 23:27:31 +0800

    Push working code

--- src/components/CDeviceListItem.vue
+++ src/components/CDeviceListItem.vue
@@ -1,13 +1,51 @@
 <template>
   <q-item>
     <q-item-section avatar>
-      <q-icon />
+      <q-icon v-if="isOnline" name="circle" data-cy="online-ind" />
+      <q-icon v-else name="radio_button_unchecked" data-cy="offline-ind" />
     </q-item-section>
   </q-item>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { DateTime } from 'luxon'
+import { computed, defineComponent } from 'vue'
 
-export default defineComponent({})
+const DEFAULT_OFFLINE_THRESHOLD = 5000
+
+export default defineComponent({
+  props: {
+    refDt: {
+      type: DateTime,
+      default: () => DateTime.now(),
+    },
+
+    /**
+     * If `refDt` is greater than `lastHeartbeatDt` by this amount (in milliseconds),
+     * the device will be considered as offline.
+     */
+    heartbeatLapseThreshold: {
+      type: Number,
+      default: DEFAULT_OFFLINE_THRESHOLD,
+    },
+
+    lastHeartbeatDt: {
+      type: DateTime,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const isOnline = computed(() => {
+      return (
+        props.refDt.diff(props.lastHeartbeatDt).milliseconds <=
+        props.heartbeatLapseThreshold
+      )
+    })
+
+    return {
+      isOnline,
+    }
+  },
+})
 </script>
