1a3295c6a464cf0fec83ab6702ef6bafdc33f9f2
Felix <jonfelixrico@gmail.com>
2022-04-03 00:47:15 +0800

    Create CDeviceList

---
+++ src/components/CDeviceList.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-list>
+    <CDeviceListItem
+      v-for="device of devices"
+      :refDt="refDt"
+      :heartbeatLapseThreshold="heartbeatLapseThreshold"
+      :device="device"
+      :key="device.deviceId"
+      :lastHeartbeatDt="lastHeartbeats[device.deviceId] ?? EPOCH"
+      clickable
+      @click="$emit('click', device)"
+      :data-cy="`device-${device.deviceId}`"
+    />
+  </q-list>
+</template>
+
+<script lang="ts">
+import { DateTime } from 'luxon'
+import { Device } from 'src/types/device.interface'
+import { defineComponent, PropType } from 'vue'
+import CDeviceListItem from './CDeviceListItem.vue'
+import { EPOCH } from 'utils/date.utils'
+
+export default defineComponent({
+  components: {
+    CDeviceListItem,
+  },
+
+  props: {
+    refDt: DateTime,
+    heartbeatLapseThreshold: Number,
+
+    devices: {
+      type: Array as PropType<Array<Device>>,
+      default: () => [],
+    },
+
+    lastHeartbeats: {
+      type: Object as PropType<Record<string, DateTime>>,
+      default: () => ({}),
+    },
+  },
+
+  setup() {
+    return {
+      EPOCH,
+    }
+  },
+
+  emits: ['click'],
+})
+</script>
