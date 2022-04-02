abf18828bdddfe299ec728771eaad0884ebb4837
Felix <jonfelixrico@gmail.com>
2022-04-03 00:58:23 +0800

    Create tests for CDeviceList

---
+++ test/cypress/__tests__/CDeviceList.spec.ts
@@ -0,0 +1,44 @@
+import { Device } from 'types/device.interface'
+import { mount } from '@cypress/vue'
+import CDeviceList from 'components/CDeviceList.vue'
+import { DateTime } from 'luxon'
+
+describe('CDeviceList', () => {
+  const devices: Device[] = [
+    {
+      deviceId: 'device-1',
+      firmwareVersion: '1',
+      modules: [],
+    },
+    {
+      deviceId: 'device-2',
+      firmwareVersion: '1',
+      modules: [],
+    },
+    {
+      deviceId: 'device-3',
+      firmwareVersion: '1',
+      modules: [],
+    },
+  ]
+
+  const lastHeartbeats: Record<string, DateTime> = {}
+
+  const props = {
+    devices,
+    lastHeartbeats,
+    refDt: DateTime.fromISO('2022-01-01T00:00:00Z'),
+    heartbeatLapseThreshold: 5000,
+  }
+
+  it('should list down all devices', () => {
+    mount(CDeviceList, {
+      props,
+    })
+
+    cy.dataCy('list-item').should('have.length', 3)
+    cy.get('[data-device-id="device-1"]').should('exist')
+    cy.get('[data-device-id="device-2"]').should('exist')
+    cy.get('[data-device-id="device-3"]').should('exist')
+  })
+})
