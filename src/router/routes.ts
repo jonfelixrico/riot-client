fa1d195692dca5af355cf72a340cbb0cdfcb4cba
Felix <jonfelixrico@gmail.com>
2022-04-03 03:20:56 +0800

    Create placeholder for device details page

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -11,6 +11,11 @@ const routes: RouteRecordRaw[] = [
         name: 'device-list',
         component: () => import('src/pages/DeviceList.vue'),
       },
+      {
+        path: 'devices/:deviceId',
+        name: 'device-details',
+        component: () => import('src/pages/DeviceDetails.vue'),
+      },
     ],
   },
 
