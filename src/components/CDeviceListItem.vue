6a234f1691a1384288d9bc26393a9cf936c8176e
Felix <jonfelixrico@gmail.com>
2022-04-02 22:45:06 +0800

    Push bare CDeviceListItem

---
+++ src/components/CDeviceListItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-item>
+    <q-item-section avatar>
+      <q-icon />
+    </q-item-section>
+  </q-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({})
+</script>
