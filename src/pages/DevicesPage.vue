cb603020c3b2ca26536be22d644e7c682918ef7e
Felix <jonfelixrico@gmail.com>
2022-04-03 01:16:00 +0800

    Push page component

---
+++ src/pages/DevicesPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page>
+    <CDeviceList />
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CDeviceList from 'components/CDeviceList.vue'
+
+export default defineComponent({
+  components: {
+    CDeviceList,
+  },
+})
+</script>
