cd0f999b8238d9ff839caea971edcf98e31b4448
Felix <jonfelixrico@gmail.com>
2022-04-03 03:36:17 +0800

    Push initial impl of DeviceDetails

--- src/pages/DeviceDetails.vue
+++ src/pages/DeviceDetails.vue
@@ -1,9 +1,43 @@
 <template>
-  <q-page></q-page>
+  <q-page class="row justify-center bg-grey-3">
+    <div class="content column q-pa-md q-gutter-y-sm">
+      <div class="row items-center q-gutter-x-sm">
+        <q-btn icon="arrow_back" flat dense round @click="onBackClick" />
+      </div>
+      <q-card class="col" flat>
+        <q-card-content>
+          <!-- TODO add content here -->
+        </q-card-content>
+      </q-card>
+    </div>
+  </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
 
-export default defineComponent({})
+export default defineComponent({
+  setup() {
+    const router = useRouter()
+
+    function onBackClick() {
+      router.push({ name: 'device-list' })
+    }
+
+    return {
+      onBackClick,
+    }
+  },
+})
 </script>
+
+<style lang="scss" scoped>
+.content {
+  body.screen--lg,
+  body.screen--md,
+  body.screen--xl & {
+    width: 1024px;
+  }
+}
+</style>
