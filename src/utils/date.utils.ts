79bf3be799f2855a127b051b695c74534dbfc44c
Felix <jonfelixrico@gmail.com>
2022-04-03 16:51:35 +0800

    Create getRelativeFormattingToken

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -1,3 +1,14 @@
 import { DateTime } from 'luxon'
 
 export const EPOCH = DateTime.fromMillis(0)
+
+export function getRelativeFormattingToken(
+  toFormat: DateTime,
+  relativeTo: DateTime
+) {
+  if (toFormat.hasSame(relativeTo, 'day')) {
+    return DateTime.TIME_SIMPLE
+  }
+
+  return DateTime.DATETIME_MED_WITH_SECONDS
+}
