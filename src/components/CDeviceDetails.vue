993b30d6e3bd55d305a5e07430f08d302140dc40
Felix <jonfelixrico@gmail.com>
2022-04-03 16:14:02 +0800

    Push initial code for CDeviceDetails

---
+++ src/components/CDeviceDetails.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-card>
+    <q-card-section>
+      <CDeviceHeader
+        :device="device"
+        :refDt="refDt"
+        :heartbeatLapseThreshold="heartbeatLapseThreshold"
+        :lastHeartbeatDt="lastHeartbeatDt"
+      />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { Device } from 'types/device.interface'
+import { DateTime } from 'luxon'
+
+export default defineComponent({
+  props: {
+    device: {
+      required: true,
+      type: Object as PropType<Device>,
+    },
+
+    refDt: DateTime,
+
+    /**
+     * If `refDt` is greater than `lastHeartbeatDt` by this amount (in milliseconds),
+     * the device will be considered as offline.
+     */
+    heartbeatLapseThreshold: Number,
+
+    lastHeartbeatDt: {
+      type: DateTime,
+      required: true,
+    },
+  },
+})
+</script>
