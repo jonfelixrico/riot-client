e68c112eee0a3bd91fb74de3a5014d8475a1bd20
Felix <jonfelixrico@gmail.com>
2022-04-03 13:06:38 +0800

    Push code for passing the online indicator tests

--- src/components/CDeviceCard.vue
+++ src/components/CDeviceCard.vue
@@ -9,7 +9,10 @@
           <h5 v-else data-cy="no-alias">No Alias</h5>
         </div>
 
-        <q-badge data-cy="online-ind" color="green">ONLINE</q-badge>
+        <q-badge v-if="isOnline" data-cy="online-ind" color="green">
+          ONLINE
+        </q-badge>
+        <q-badge v-else data-cy="offline-ind" color="grey">OFFLINE</q-badge>
       </div>
 
       <div class="row q-gutter-x-sm">
@@ -28,8 +31,12 @@
 </template>
 
 <script lang="ts">
+import { computed } from '@vue/reactivity'
+import { DateTime } from 'luxon'
 import { Device } from 'src/types/device.interface'
 import { defineComponent, PropType } from 'vue'
+
+const DEFAULT_OFFLINE_THRESHOLD = 5000
 
 export default defineComponent({
   props: {
@@ -37,6 +44,38 @@ export default defineComponent({
       required: true,
       type: Object as PropType<Device>,
     },
+
+    refDt: {
+      type: DateTime,
+      default: () => DateTime.now(),
+    },
+
+    /**
+     * If `refDt` is greater than `lastHeartbeatDt` by this amount (in milliseconds),
+     * the device will be considered as offline.
+     */
+    heartbeatLapseThreshold: {
+      type: Number,
+      default: DEFAULT_OFFLINE_THRESHOLD,
+    },
+
+    lastHeartbeatDt: {
+      type: DateTime,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const isOnline = computed(() => {
+      return (
+        props.refDt.diff(props.lastHeartbeatDt).milliseconds <=
+        props.heartbeatLapseThreshold
+      )
+    })
+
+    return {
+      isOnline,
+    }
   },
 })
 </script>
