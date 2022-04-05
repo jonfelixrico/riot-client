2cbcc694d3ac2aa0235d6e2641cb4cfcbdab7551
Felix <jonfelixrico@gmail.com>
2022-04-06 00:24:33 +0800

    Create CDateDisplay

---
+++ src/components/common/CDateDisplay.vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-if="relative">
+    <span class="cursor-pointer text-underline-dotted">{{ relative }}</span>
+    <q-tooltip>{{ absolute }}</q-tooltip>
+  </div>
+
+  <div v-else>
+    {{ absolute }}
+  </div>
+</template>
+
+<script lang="ts">
+import { computed } from '@vue/reactivity'
+import { DateTime } from 'luxon'
+import { useStaticDateTime } from 'src/composables/static-datetime.composable'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    date: {
+      type: DateTime,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const now = useStaticDateTime()
+
+    const relative = computed(() => {
+      if (!props.date.hasSame(now, 'day')) {
+        return null
+      }
+
+      return props.date.toLocaleString(DateTime.TIME_WITH_SECONDS)
+    })
+
+    const absolute = computed(() =>
+      props.date.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)
+    )
+
+    return {
+      relative,
+      absolute,
+    }
+  },
+})
+</script>
