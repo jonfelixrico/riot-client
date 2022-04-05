9f8a1292cf42e8df2a1f5ec6292c95a5f791a46b
Felix <jonfelixrico@gmail.com>
2022-04-05 23:06:12 +0800

    Add i18n

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -1,7 +1,5 @@
-// This is just an example,
-// so you can safely delete all default props below
+import registration from './registration'
 
 export default {
-  failed: 'Action failed',
-  success: 'Action was successful',
+  registration,
 }
