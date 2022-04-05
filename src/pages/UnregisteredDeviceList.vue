56878d3b897f15679c60a9d62f2e8b684c7e411b
Felix <jonfelixrico@gmail.com>
2022-04-06 01:03:05 +0800

    Create stub for unregisteredDeviceList

---
+++ src/pages/UnregisteredDeviceList.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page>
+    <CUnregisteredDeviceListItem
+      v-for="device of devices"
+      :key="[device.deviceId, device.firmwareVersion].join('/')"
+      :device="device"
+    />
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import CUnregisteredDeviceListItem from 'components/registration/CUnregisteredDeviceListItem.vue'
+import { useUnregisteredListApi } from 'composables/unregistered-api.composable'
+
+export default defineComponent({
+  components: {
+    CUnregisteredDeviceListItem,
+  },
+
+  setup() {
+    const { devices } = useUnregisteredListApi()
+    return { devices }
+  },
+})
+</script>
