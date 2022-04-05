e436d174dc6f4f88a2e61a486d91d3a16cdd09d0
Felix <jonfelixrico@gmail.com>
2022-04-05 23:23:09 +0800

    Add partially-passing implementation

--- src/components/unregistered-devices/CUnregisteredDeviceListItem.vue
+++ src/components/unregistered-devices/CUnregisteredDeviceListItem.vue
@@ -1,11 +1,57 @@
 <template>
   <q-card>
-    <q-card-section></q-card-section>
+    <q-card-section class="row items-center">
+      <div class="col">
+        <div role="heading" aria-level="6" class="text-h6" data-cy="device-id">
+          {{ device.deviceId }}
+        </div>
+        <div class="row q-gutter-x-sm">
+          <span class="text-caption text-grey-7" data-cy="version">
+            {{ t('common.version', { version: device.firmwareVersion }) }}
+          </span>
+
+          <q-separator vertical />
+
+          <span class="text-caption text-grey-7" data-cy="last-activity">
+            {{ t('registration.lastActivity', { date: 'PLACEHOLDER' }) }}
+          </span>
+        </div>
+      </div>
+      <q-btn
+        data-cy="register-btn"
+        no-caps
+        @click="$emit('register-click')"
+        color="primary"
+        unelevated
+      >
+        {{ t('registration.register') }}
+      </q-btn>
+    </q-card-section>
   </q-card>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { DateTime } from 'luxon'
+import { UnregisteredDevice } from 'src/types/unregistered-device.interface'
+import { defineComponent, PropType } from 'vue'
+import { useI18n } from 'vue-i18n'
 
-export default defineComponent({})
+export default defineComponent({
+  emits: ['register-click'],
+
+  props: {
+    device: {
+      required: true,
+      type: Object as PropType<UnregisteredDevice>,
+    },
+  },
+
+  setup() {
+    const { t } = useI18n()
+
+    return {
+      t,
+    }
+  },
+})
 </script>
