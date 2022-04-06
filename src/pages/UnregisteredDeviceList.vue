ef8a1470ec266c2ac357a3ffeb4badf0467f59b9
Felix <jonfelixrico@gmail.com>
2022-04-07 00:34:32 +0800

    Create UI for empty list

--- src/pages/UnregisteredDeviceList.vue
+++ src/pages/UnregisteredDeviceList.vue
@@ -1,19 +1,44 @@
 <template>
-  <q-page class="page-width q-mx-auto">
-    <CUnregisteredDeviceListItem
-      v-for="device of devices"
-      :key="[device.deviceId, device.firmwareVersion].join('/')"
-      :device="device"
-      data-cy="device"
-      :data-device-id="device.deviceId"
-    />
+  <q-page class="row justify-center">
+    <div class="page-width" v-if="devices.length">
+      <div class="row justify-end">
+        <q-btn
+          dense
+          flat
+          round
+          icon="refresh"
+          :loading="isLoading"
+          @click="fetch"
+        />
+      </div>
+
+      <CUnregisteredDeviceListItem
+        v-for="device of devices"
+        :key="[device.deviceId, device.firmwareVersion].join('/')"
+        :device="device"
+        data-cy="device"
+        :data-device-id="device.deviceId"
+      />
+    </div>
+
+    <q-card v-else class="page-width q-ma-md flex flex-center" flat>
+      <div class="column items-center q-gutter-y-sm">
+        <div role="heading" aria-level="3" class="text-h5">
+          {{ t('registration.list.empty') }}
+        </div>
+        <q-btn color="primary" unelevated no-caps>
+          {{ t('common.refresh') }}
+        </q-btn>
+      </div>
+    </q-card>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, onBeforeMount } from 'vue'
 import CUnregisteredDeviceListItem from 'components/registration/CUnregisteredDeviceListItem.vue'
 import { useUnregisteredListApi } from 'composables/unregistered-api.composable'
+import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   components: {
@@ -21,8 +46,17 @@ export default defineComponent({
   },
 
   setup() {
-    const { devices } = useUnregisteredListApi()
-    return { devices }
+    const { devices, fetch, isLoading } = useUnregisteredListApi()
+    const { t } = useI18n()
+
+    onBeforeMount(fetch)
+
+    return {
+      devices,
+      isLoading,
+      fetch,
+      t,
+    }
   },
 })
 </script>
