94329cd399530043031d2a642ed7dc51d5f60fab
Felix <jonfelixrico@gmail.com>
2022-04-07 01:45:38 +0800

    Create common dialog component for confirmations

---
+++ src/components/common/CConfirmationDialog.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <q-card class="q-dialog-plugin" :data-cy="dataCy">
+      <q-card-section>
+        <div role="heading" aria-level="1" v-text="title" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div v-text="message">
+      </q-card-section>
+
+      <q-card-actions align="right">
+        <q-btn color="primary" unelevated no-caps :label="t('common.ok')" @click="onOKClick" data-cy="ok" />
+        <q-btn flat no-caps :label="t('common.cancel')" @click="onCancelClick" data-cy="cancel" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+import { useDialogPluginComponent } from 'quasar'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    title: String,
+    message: String,
+    dataCy: String
+  },
+
+  emits: [...useDialogPluginComponent.emits],
+
+  setup() {
+    const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } =
+      useDialogPluginComponent()
+    const { t } = useI18n()
+
+    return {
+      dialogRef,
+      onDialogHide,
+      onOKClick: onDialogOK,
+      onCancelClick: onDialogCancel,
+      t
+    }
+  },
+})
+</script>
