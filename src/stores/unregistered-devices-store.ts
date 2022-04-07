2c36ca116cfe659e157b5309e697b989bda1757f
Felix <jonfelixrico@gmail.com>
2022-04-08 00:25:09 +0800

    Create unregistered devices store

---
+++ src/stores/unregistered-devices-store.ts
@@ -0,0 +1,29 @@
+import { defineStore } from 'pinia'
+import { DeviceIdentifier } from 'src/types/device.interface'
+import { UnregisteredDevice } from 'src/types/unregistered-device.interface'
+
+interface UnregisteredDevicesState {
+  devices: UnregisteredDevice[]
+}
+
+export const useCounterStore = defineStore('unregisteredDevices', {
+  state: (): UnregisteredDevicesState => ({
+    devices: [],
+  }),
+
+  actions: {
+    setDevices(devices: UnregisteredDevice[]) {
+      this.devices = devices
+    },
+
+    removeDevice({ deviceId, firmwareVersion }: DeviceIdentifier): void {
+      const { devices } = this
+
+      const index = devices.findIndex(
+        (d) => d.deviceId === deviceId && d.firmwareVersion === firmwareVersion
+      )
+
+      devices.splice(index, 1)
+    },
+  },
+})
