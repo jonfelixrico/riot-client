3068d95c54c2c0004dc559b4174207cff79f1b5f
Felix <jonfelixrico@gmail.com>
2022-04-09 20:25:54 +0800

    Pass the tests

--- src/components/relay/CRelayVerticalBar.vue
+++ src/components/relay/CRelayVerticalBar.vue
@@ -1,9 +1,64 @@
 <template>
-  <div />
+  <div :style="{ width: `${width}px`, height: `${height}px` }">
+    <div
+      v-for="({ start, end, state }, index) of intervals"
+      :key="index"
+      data-cy="interval"
+      :data-state="state"
+      :data-start="start"
+      :data-end="end"
+      :style="{ width: `${width}px`, height: `${height}px` }"
+    />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { DisplaySchedule } from 'src/utils/daily-relay-schedule.utils'
+import { TimeUnit } from 'src/utils/relay-schedule.utils'
+import { computed, defineComponent, PropType } from 'vue'
 
-export default defineComponent({})
+interface Interval extends DisplaySchedule {
+  startSeconds: number
+  endSeconds: number
+}
+
+function convert({ hour, second, minute }: TimeUnit) {
+  return hour * 3600 + minute * 60 + second
+}
+export default defineComponent({
+  props: {
+    schedule: {
+      type: Array as PropType<DisplaySchedule[]>,
+      required: true,
+    },
+
+    currentTime: Object as PropType<TimeUnit>,
+
+    width: {
+      type: Number,
+      default: 200,
+    },
+
+    height: {
+      type: Number,
+      default: 300,
+    },
+  },
+
+  setup(props) {
+    const intervals = computed<Interval[]>(() =>
+      props.schedule.map((sched) => {
+        return {
+          ...sched,
+          startSeconds: convert(sched.start),
+          endSeconds: convert(sched.end),
+        }
+      })
+    )
+
+    return {
+      intervals,
+    }
+  },
+})
 </script>
