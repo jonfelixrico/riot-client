7ef6cdad648b525d0b0935b0880a7a5de8157295
Felix <jonfelixrico@gmail.com>
2022-04-10 21:50:13 +0800

    Add comment in ScheduleBarItem

--- src/components/relay/schedule-bar.types.ts
+++ src/components/relay/schedule-bar.types.ts
@@ -1,8 +1,20 @@
 import { RelayState } from 'src/utils/relay-schedule.utils'
 
 export interface ScheduleBarItem {
+  /**
+   * Time unit represented as seconds.
+   */
   start: number
+
+  /**
+   * Time unit represented as seconds.
+   */
   end: number
+
   id: string
-  state: RelayState | number
+
+  /**
+   * A state of null means that the scheduel item is unoccupied.
+   */
+  state: RelayState | null
 }
