1f288245369cddbb2c1cb163856a824d8183e3cb
Felix <jonfelixrico@gmail.com>
2022-04-12 23:54:42 +0800

    Create initial component

---
+++ src/components/relay/CRelayModuleListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <CDeviceModuleHeader :deviceModule="deviceModule" />
+    <q-separator />
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import CDeviceModuleHeader from 'components/CDeviceModuleHeader.vue'
+import { DeviceModule } from 'src/types/device.interface'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  components: { CDeviceModuleHeader },
+
+  props: {
+    deviceModule: {
+      type: Object as PropType<DeviceModule>,
+      required: true,
+    },
+  },
+})
+</script>
