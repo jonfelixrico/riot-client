0055303c8291bf4e3a16bf572520cbe14e0da76f
Felix <jonfelixrico@gmail.com>
2022-04-13 00:44:04 +0800

    Create stubs for the schedule

---
+++ src/components/relay/CDailySchedulePreview.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="relative">this is a stub</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { DailySchedule } from 'src/types/relay-config.interface'
+
+export default defineComponent({
+  props: {
+    utcOffset: {
+      type: String as PropType<DailySchedule['utcOffset']>,
+      required: true,
+    },
+
+    dailySchedule: {
+      type: Array as PropType<DailySchedule['dailySchedule']>,
+      required: true,
+    },
+  },
+})
+</script>
---
+++ src/components/relay/CWeeklySchedulePreview.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="relative">this is a stub</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { WeeklySchedule } from 'src/types/relay-config.interface'
+
+export default defineComponent({
+  props: {
+    utcOffset: {
+      type: String as PropType<WeeklySchedule['utcOffset']>,
+      required: true,
+    },
+
+    weeklySchedule: {
+      type: Object as PropType<WeeklySchedule['weeklySchedule']>,
+      required: true,
+    },
+  },
+})
+</script>
