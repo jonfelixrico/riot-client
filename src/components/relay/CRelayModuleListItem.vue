3ba96a69576adc7e6a1d1a724b179a97f89bcdbd
Felix <jonfelixrico@gmail.com>
2022-04-13 00:49:11 +0800

    Make CRelayModuleListItem dispatch between schedule components

--- src/components/relay/CRelayModuleListItem.vue
+++ src/components/relay/CRelayModuleListItem.vue
@@ -2,23 +2,61 @@
   <div>
     <CDeviceModuleHeader :deviceModule="deviceModule" />
     <q-separator />
-    <div></div>
+    <!-- TODO do not hardcode the height value -->
+    <q-skeleton v-if="!relayConfig" height="200px" />
+
+    <CDailySchedulePreview
+      v-else-if="relayConfig.schedule.type === 'DAILY'"
+      :utcOffset="relayConfig.schedule.utcOffset"
+      :dailySchedule="relayConfig.schedule.dailySchedule"
+    />
+
+    <CWeeklySchedulePreview
+      v-else-if="relayConfig.schedule.type === 'WEEKLY'"
+      :utcOffset="relayConfig.schedule.utcOffset"
+      :weeklySchedule="relayConfig.schedule.weeklySchedule"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import CDeviceModuleHeader from 'components/CDeviceModuleHeader.vue'
-import { DeviceModule } from 'src/types/device.interface'
-import { defineComponent, PropType } from 'vue'
+import { DeviceIdentifier, DeviceModule } from 'src/types/device.interface'
+import { defineComponent, onBeforeMount, PropType } from 'vue'
+import { useGetRelayConfigApi } from 'composables/get-relay-config-api.composable'
+import CDailySchedulePreview from './CDailySchedulePreview.vue'
+import CWeeklySchedulePreview from './CWeekySchedulePreview.vue'
 
 export default defineComponent({
-  components: { CDeviceModuleHeader },
+  components: {
+    CDeviceModuleHeader,
+    CDailySchedulePreview,
+    CWeeklySchedulePreview,
+  },
 
   props: {
     deviceModule: {
       type: Object as PropType<DeviceModule>,
       required: true,
     },
+
+    device: {
+      type: Object as PropType<DeviceIdentifier>,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const { relayConfig, fetch } = useGetRelayConfigApi({
+      ...props.deviceModule,
+      ...props.device,
+    })
+
+    onBeforeMount(fetch)
+
+    return {
+      relayConfig,
+    }
   },
 })
 </script>
