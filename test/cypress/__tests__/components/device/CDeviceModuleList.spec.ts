4dd7007723fd68c64b83ea4ef0d826dd0cb91cc7
Felix <jonfelixrico@gmail.com>
2022-04-14 02:09:03 +0800

    Pass the tests

--- test/cypress/__tests__/components/device/CDeviceModuleList.spec.ts
+++ test/cypress/__tests__/components/device/CDeviceModuleList.spec.ts
@@ -1,50 +1,59 @@
-import { DeviceModule } from 'src/types/device.interface'
+import { Device, DeviceModule } from 'src/types/device.interface'
 import { mount } from '@cypress/vue'
 import CDeviceModuleList from 'src/components/device-module/CDeviceModuleList.vue'
 
 describe('CDeviceModuleList', () => {
-  const deviceModules: DeviceModule[] = [
-    {
-      moduleId: 'switch-1',
-      type: 'SWITCH',
-      alias: 'Switch 1',
-    },
-    {
-      moduleId: 'switch-2',
-      type: 'SWITCH',
-      alias: 'Switch 2',
-    },
-    {
-      moduleId: 'switch-3',
-      type: 'SWITCH',
-      alias: 'Switch 3',
-    },
-    {
-      moduleId: 'switch-4',
-      type: 'SWITCH',
-      alias: 'Switch 4',
-    },
-    {
-      moduleId: 'switch-5',
-      type: 'SWITCH',
-      alias: 'Switch 5',
-    },
-    {
-      moduleId: 'ph-sensor-1',
-      type: 'PH_SENSOR',
-      alias: 'PH sensor 1',
-    },
-  ]
+  const device: Device = {
+    deviceId: 'device-1',
+    firmwareVersion: '1',
+    modules: [
+      {
+        moduleId: 'switch-1',
+        type: 'SWITCH',
+        alias: 'Switch 1',
+      },
+      {
+        moduleId: 'switch-2',
+        type: 'SWITCH',
+        alias: 'Switch 2',
+      },
+      {
+        moduleId: 'switch-3',
+        type: 'SWITCH',
+        alias: 'Switch 3',
+      },
+      {
+        moduleId: 'switch-4',
+        type: 'SWITCH',
+        alias: 'Switch 4',
+      },
+      {
+        moduleId: 'switch-5',
+        type: 'SWITCH',
+        alias: 'Switch 5',
+      },
+      {
+        moduleId: 'ph-sensor-1',
+        type: 'PH_SENSOR',
+        alias: 'PH sensor 1',
+      },
+    ],
+  }
 
   beforeEach(() => {
     mount(CDeviceModuleList, {
       props: {
-        deviceModules,
+        device,
+      },
+      global: {
+        stubs: {
+          CDeviceModuleListItem: true,
+        },
       },
     })
   })
 
   it('should render the correct number of modules', () => {
-    cy.dataCy('device-module').should('have.length', deviceModules.length)
+    cy.dataCy('device-module').should('have.length', device.modules.length)
   })
 })
