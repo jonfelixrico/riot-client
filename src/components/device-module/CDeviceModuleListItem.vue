7225d64870aa0ecda629b615c47c608aa4f30274
Felix <jonfelixrico@gmail.com>
2022-04-14 02:04:27 +0800

    Create list item component for modules

---
+++ src/components/device-module/CDeviceModuleListItem.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- For unknown types -->
+  <CDeviceModuleHeader
+    v-if="!COMPONENT_MAP[deviceModule.type]"
+    :deviceModule="deviceModule"
+  />
+
+  <component
+    v-else
+    :is="COMPONENT_MAP[deviceModule.type]"
+    :deviceModule="deviceModule"
+    :device="device"
+  />
+</template>
+
+<script lang="ts">
+import { DeviceIdentifier, DeviceModule } from 'src/types/device.interface'
+import { defineComponent, PropType } from 'vue'
+import CRelayModuleListItem from '../relay/CRelayModuleListItem.vue'
+
+// TODO try to make this load lazily; right now this is eagerly loaded
+const COMPONENT_MAP = {
+  RELAY: CRelayModuleListItem,
+}
+
+/**
+ * The puropse of this component is to dispatch between sub-components based
+ * on the type.
+ */
+export default defineComponent({
+  props: {
+    deviceModule: {
+      type: Object as PropType<DeviceModule>,
+      required: true,
+    },
+
+    device: {
+      type: Object as PropType<DeviceIdentifier>,
+      required: true,
+    },
+  },
+
+  setup() {
+    return {
+      COMPONENT_MAP,
+    }
+  },
+})
+</script>
