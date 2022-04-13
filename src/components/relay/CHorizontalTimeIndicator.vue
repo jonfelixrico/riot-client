6ffbbe92d3821261f0c6c48f4390bea601cd6297
Felix <jonfelixrico@gmail.com>
2022-04-14 01:28:55 +0800

    Create separate component for the time indicator

---
+++ src/components/relay/CHorizontalTimeIndicator.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    :style="{
+      height: `${iconSize}px`,
+    }"
+    class="relative-position"
+  >
+    <div
+      :style="[
+        {
+          height: `${iconSize}px`,
+        },
+        positionStyle,
+      ]"
+      class="absolute"
+    >
+      <q-icon name="place" :size="`${iconSize}px`" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed } from '@vue/reactivity'
+import { round } from 'lodash'
+import { DateTime } from 'luxon'
+import { defineComponent } from 'vue'
+import { convertDateTimeToSeconds } from './relay-schedule-presentation.utils'
+import { MAX_SECONDS } from './relay.constants'
+
+export default defineComponent({
+  props: {
+    width: {
+      type: Number,
+      required: true,
+    },
+
+    now: {
+      type: DateTime,
+      required: true,
+    },
+
+    iconSize: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const positionStyle = computed(() => {
+      const leftOffset = props.iconSize / 2
+      const left =
+        (convertDateTimeToSeconds(props.now) / MAX_SECONDS) * props.width
+      return {
+        left: `${round(left) - leftOffset}px`,
+      }
+    })
+
+    return {
+      positionStyle,
+    }
+  },
+})
+</script>
