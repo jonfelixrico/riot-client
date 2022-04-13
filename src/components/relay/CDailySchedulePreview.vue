8f37f8e44f0b0176641b131956d40d881e23120f
Felix <jonfelixrico@gmail.com>
2022-04-14 00:49:37 +0800

    Finish working CDailySchedulePreview

--- src/components/relay/CDailySchedulePreview.vue
+++ src/components/relay/CDailySchedulePreview.vue
@@ -1,16 +1,19 @@
 <template>
-  <CSchedulePreview
-    :originalUtcOffset="utcOffset"
-    :scheduleEntries="dailySchedule"
-    :now="now"
-  />
+  <CHorizontalSchedulePreview :now="now" :entries="transformed" />
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, computed } from 'vue'
 import { DailySchedule } from 'src/types/relay-config.interface'
-import CSchedulePreview from './CSchedulePreview.vue'
+import CHorizontalSchedulePreview from './CHorizontalSchedulePreview.vue'
 import { useTickingDateTime } from 'src/composables/ticking-datetime.composable'
+import { processScheduleEntries } from 'src/utils/relay-schedule.utils'
+import {
+  transformForPresentation,
+  fillGapsInSchedule,
+  PresentationScheduleEntry,
+} from './relay-schedule-presentation.utils'
+import { sortBy } from 'lodash'
 
 export default defineComponent({
   props: {
@@ -23,13 +26,49 @@ export default defineComponent({
       required: true,
     },
   },
-  components: { CSchedulePreview },
+  components: { CHorizontalSchedulePreview },
 
-  setup() {
+  setup(props) {
     const { now } = useTickingDateTime()
+
+    const transformed = computed(() => {
+      const { dailySchedule, utcOffset } = props
+      const processed = processScheduleEntries(dailySchedule, utcOffset)
+      const transformed = transformForPresentation(processed)
+      const sorted = sortBy(transformed, 'start')
+
+      const merged = sorted.reduce<PresentationScheduleEntry[]>(
+        (arr, entry) => {
+          if (!arr.length) {
+            arr.push(entry)
+            return arr
+          }
+
+          const lastItemIdx = arr.length - 1
+          const lastItem = arr[lastItemIdx]
+          if (
+            lastItem.state === entry.state &&
+            entry.start - lastItem.end === 1
+          ) {
+            arr.splice(lastItemIdx, 1, {
+              ...lastItem,
+              end: entry.end,
+            })
+          } else {
+            arr.push(entry)
+          }
+
+          return arr
+        },
+        []
+      )
+
+      return fillGapsInSchedule(merged)
+    })
 
     return {
       now,
+      transformed,
     }
   },
 })
