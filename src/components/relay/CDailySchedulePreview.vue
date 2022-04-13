0788b9188245158b47223e361ff49cd51dcd3981
Felix <jonfelixrico@gmail.com>
2022-04-14 00:08:38 +0800

    Integrate CSchedulePreview into CDailySchedulePreview

--- src/components/relay/CDailySchedulePreview.vue
+++ src/components/relay/CDailySchedulePreview.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="relative">this is a stub for daily</div>
+  <CSchedulePreview
+    :originalUtcOffset="utcOffset"
+    :scheduleEntries="dailySchedule"
+    :now="now"
+  />
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import { DailySchedule } from 'src/types/relay-config.interface'
+import CSchedulePreview from './CSchedulePreview.vue'
+import { useTickingDateTime } from 'src/composables/ticking-datetime.composable'
 
 export default defineComponent({
   props: {
@@ -12,11 +18,19 @@ export default defineComponent({
       type: String as PropType<DailySchedule['utcOffset']>,
       required: true,
     },
-
     dailySchedule: {
       type: Array as PropType<DailySchedule['dailySchedule']>,
       required: true,
     },
+  },
+  components: { CSchedulePreview },
+
+  setup() {
+    const { now } = useTickingDateTime()
+
+    return {
+      now,
+    }
   },
 })
 </script>
