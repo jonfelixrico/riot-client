60a6ff903978962e0899f8a0bdf36557d18b7395
Felix <jonfelixrico@gmail.com>
2022-04-15 01:34:07 +0800

    Create tests

--- test/cypress/__tests__/components/relay/CWeeklySchedulePreview.spec.ts
+++ test/cypress/__tests__/components/relay/CWeeklySchedulePreview.spec.ts
@@ -1,3 +1,62 @@
+import { DateTime } from 'luxon'
+import { WeeklySchedule } from 'src/types/relay-config.interface'
+import { timeStringToScheduleEntry } from './relay-schedule.test-utils'
+import { mount } from '@cypress/vue'
+import CWeeklySchedulePreview from 'components/relay/CWeeklySchedulePreview.vue'
+import { i18n } from 'src/boot/i18n'
+
+const scheduleArr = [
+  timeStringToScheduleEntry('00:00:00', '12:00:00', 'ON'),
+  timeStringToScheduleEntry('12:00:01', '23:59:59', 'ON'),
+]
+
+const weeklySchedule: WeeklySchedule['weeklySchedule'] = {
+  mon: scheduleArr,
+  tues: scheduleArr,
+  wed: scheduleArr,
+  thurs: scheduleArr,
+  fri: scheduleArr,
+  sat: scheduleArr,
+  sun: scheduleArr,
+}
+
+const props = {
+  weeklySchedule,
+  utcOffset: '+8',
+  now: DateTime.fromISO('2022-01-05T00:00:00+08:00', {
+    // wednesday
+    setZone: true,
+  }),
+}
+
 describe('CWeeklySchedulePreview', () => {
-  it('stub')
+  beforeEach(() => {
+    mount(CWeeklySchedulePreview, {
+      props,
+      global: {
+        plugins: [i18n],
+      },
+    })
+  })
+
+  it('should have a visual representation for each day', () => {
+    cy.dataCy('schedule-preview').should('have.length', 7)
+    cy.dataCy('schedule-preview').get('[data-day="mon"]').should('exist')
+    cy.dataCy('schedule-preview').get('[data-day="tues"]').should('exist')
+    cy.dataCy('schedule-preview').get('[data-day="wed"]').should('exist')
+    cy.dataCy('schedule-preview').get('[data-day="thurs"]').should('exist')
+    cy.dataCy('schedule-preview').get('[data-day="fri"]').should('exist')
+    cy.dataCy('schedule-preview').get('[data-day="sat"]').should('exist')
+    cy.dataCy('schedule-preview').get('[data-day="sun"]').should('exist')
+  })
+
+  it('should indicate the current day', () => {
+    cy.dataCy('schedule-preview')
+      .get('[data-day="wed"]')
+      .should('have.attr', 'data-current-day', 'true')
+
+    cy.dataCy('schedule-preview')
+      .get('[data-current-day="true"]')
+      .should('have.length', 1)
+  })
 })
