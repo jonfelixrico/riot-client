03c77cefc202f3a8559b1b97f8665efbf31230be
Felix <jonfelixrico@gmail.com>
2022-04-15 00:52:49 +0800

    Create additional utility methods

--- test/cypress/__tests__/components/relay/relay-schedule.test-utils.ts
+++ test/cypress/__tests__/components/relay/relay-schedule.test-utils.ts
@@ -1,5 +1,9 @@
 import { PresentationScheduleEntry } from 'src/components/relay/relay-schedule-presentation.utils'
-import { RelayState } from 'src/types/relay-config.interface'
+import {
+  RelayState,
+  ScheduleEntry,
+  TimeUnit,
+} from 'src/types/relay-config.interface'
 
 function timeStringToSeconds(timeStr: string): number {
   const [hour, minute, second] = timeStr.split(':').map(Number)
@@ -16,5 +20,27 @@ export function timeStringToPresentationScheduleEntry(
     start: timeStringToSeconds(startStr),
     end: timeStringToSeconds(endStr),
     state: state ?? null,
+  }
+}
+
+function timeStringToTimeUnit(timeStr: string): TimeUnit {
+  const [hour, minute, second] = timeStr.split(':').map(Number)
+
+  return {
+    hour,
+    minute,
+    second,
+  }
+}
+
+export function timeStringToScheduleEntry(
+  startStr: string,
+  endStr: string,
+  state: RelayState
+): ScheduleEntry {
+  return {
+    start: timeStringToTimeUnit(startStr),
+    end: timeStringToTimeUnit(endStr),
+    state: state,
   }
 }
