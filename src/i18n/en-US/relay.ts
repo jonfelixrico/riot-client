ced83dfb9769e11f2e84f2a055adc864b282c52d
Felix <jonfelixrico@gmail.com>
2022-04-14 18:44:45 +0800

    Create relay-related i18n strings

---
+++ src/i18n/en-US/relay.ts
@@ -0,0 +1,11 @@
+export default {
+  daysOfWeek: {
+    mon: 'Monday',
+    tues: 'Tuesday',
+    wed: 'Wednesday',
+    thurs: 'Thursday',
+    fri: 'Friday',
+    sat: 'Saturday',
+    sun: 'Sunday',
+  },
+}
