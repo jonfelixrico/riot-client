37e29cdcc00bf3e9a3e9d076e0c653197fef3dc6
Felix <jonfelixrico@gmail.com>
2022-04-15 17:19:29 +0800

    Push deleting and displaying logic

--- src/components/relay-edit/CScheduleEdit.vue
+++ src/components/relay-edit/CScheduleEdit.vue
@@ -6,6 +6,9 @@
 import { defineComponent, PropType, ref, watch } from 'vue'
 import { ScheduleEntry } from 'types/relay-config.interface'
 import { cloneDeep } from 'lodash'
+import { computed } from '@vue/reactivity'
+import { transformScheduleEntryForPresentation } from '../relay/relay-schedule-presentation.utils'
+import { useScheduleEntryDeleteHandler } from './schedule-entry-delete-handler.composable'
 
 export default defineComponent({
   props: {
@@ -23,6 +26,17 @@ export default defineComponent({
     watch(props.modelValue, () => {
       snapshot.value = cloneDeep(props.modelValue)
     })
+
+    const forPresentation = computed(() => {
+      return snapshot.value.map(transformScheduleEntryForPresentation)
+    })
+
+    const { handleDelete } = useScheduleEntryDeleteHandler(snapshot)
+
+    return {
+      forPresentation,
+      handleDelete,
+    }
   },
 })
 </script>
