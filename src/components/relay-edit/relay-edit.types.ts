1173f55defe756a5f82d703e3b1c1a9835df07b6
Felix <jonfelixrico@gmail.com>
2022-04-15 17:45:40 +0800

    Add comment regarding the importance of id

--- src/components/relay-edit/relay-edit.types.ts
+++ src/components/relay-edit/relay-edit.types.ts
@@ -1,5 +1,9 @@
 import { ScheduleEntry } from 'src/types/relay-config.interface'
 
 export interface ScheduleEntryForEditing extends ScheduleEntry {
+  /**
+   * Just an internal id for editing-related components. It helps us differentiate
+   * items better than just relying on indexes.
+   */
   id: string
 }
