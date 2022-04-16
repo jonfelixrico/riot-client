13971d0c1fc6d5f43f5d40b3c838c7bee53a7f5e
Felix <jonfelixrico@gmail.com>
2022-04-17 03:28:30 +0800

    Create component for the set schedule dialog

---
+++ src/components/relay-details/CSetScheduleDialog.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <q-card class="q-dialog-plugin">
+      <q-card-actions align="right">
+        <q-btn color="primary" label="OK" @click="onDialogOK" />
+        <q-btn color="primary" label="Cancel" @click="onDialogCancel" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+import { useDialogPluginComponent } from 'quasar'
+
+export default {
+  emits: [...useDialogPluginComponent.emits],
+
+  setup() {
+    const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } =
+      useDialogPluginComponent()
+
+    return {
+      dialogRef,
+      onDialogHide,
+      onDialogOK,
+      onDialogCancel,
+    }
+  },
+}
+</script>
