830d57778eb49f233885e322826ce5daebd8ffde
Felix <jonfelixrico@gmail.com>
2022-04-17 03:34:54 +0800

    Adjust CSetScheduleDialog to use i18n strings

--- src/components/relay-details/CSetScheduleDialog.vue
+++ src/components/relay-details/CSetScheduleDialog.vue
@@ -2,8 +2,21 @@
   <q-dialog ref="dialogRef" @hide="onDialogHide">
     <q-card class="q-dialog-plugin">
       <q-card-actions align="right">
-        <q-btn color="primary" label="OK" @click="onDialogOK" />
-        <q-btn color="primary" label="Cancel" @click="onDialogCancel" />
+        <q-btn
+          color="primary"
+          no-caps
+          unelevated
+          dense
+          :label="t('common.ok')"
+          @click="onDialogOK"
+        />
+        <q-btn
+          no-caps
+          flat
+          dense
+          :label="t('common.cancel')"
+          @click="onDialogCancel"
+        />
       </q-card-actions>
     </q-card>
   </q-dialog>
@@ -11,6 +24,7 @@
 
 <script>
 import { useDialogPluginComponent } from 'quasar'
+import { useI18n } from 'vue-i18n'
 
 export default {
   emits: [...useDialogPluginComponent.emits],
@@ -19,11 +33,14 @@ export default {
     const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } =
       useDialogPluginComponent()
 
+    const { t } = useI18n()
+
     return {
       dialogRef,
       onDialogHide,
       onDialogOK,
       onDialogCancel,
+      t,
     }
   },
 }
