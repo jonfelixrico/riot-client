e3ebdde72c2ff75ce12773dde802944ed49fd2da
Felix <jonfelixrico@gmail.com>
2022-04-17 02:33:16 +0800

    Create stub for schedule details

---
+++ src/components/relay-details/CRelayScheduleDetails.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>stub</div>
+</template>
+
+<script lang="ts">
+import { DailySchedule, WeeklySchedule } from 'src/types/relay-config.interface'
+import { defineComponent, PropType } from 'vue'
+
+type Schedule = {
+  utcOffset: string
+} & (DailySchedule | WeeklySchedule)
+
+export default defineComponent({
+  props: {
+    modelValue: {
+      type: Object as PropType<Schedule>,
+      required: true,
+    },
+  },
+
+  emits: ['update:modelValue'],
+})
+</script>
