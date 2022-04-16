31615963f0e9080e6f09acdcc05e246db104fe0e
Felix <jonfelixrico@gmail.com>
2022-04-17 03:00:16 +0800

    Implement listing

--- src/components/relay-details/CScheduleDetails.vue
+++ src/components/relay-details/CScheduleDetails.vue
@@ -18,8 +18,25 @@
 
     <template #default>
       <div class="row">
-        <div class="col">
-          <!-- list of schedule goes here -->
+        <div class="col q-gutter-y-sm">
+          <div
+            v-for="{ entry, i18nParts } in forPresentation"
+            :key="`${entry.start}-${entry.end}`"
+          >
+            <i18n-t keypath="relay.timeSlotLabel">
+              <template #start>
+                <span class="text-weight-bold">{{ i18nParts.start }}</span>
+              </template>
+
+              <template #end>
+                <span class="text-weight-bold">{{ i18nParts.end }}</span>
+              </template>
+
+              <template #state>
+                <span class="text-weight-bold">{{ i18nParts.state }}</span>
+              </template>
+            </i18n-t>
+          </div>
         </div>
         <div>
           <!-- edit button goes here -->
@@ -30,9 +47,24 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType, ref } from 'vue'
+import { computed, defineComponent, PropType, ref } from 'vue'
 import { PresentationScheduleEntry } from '../relay/relay-schedule-presentation.utils'
 import CScheduleDisplay from 'components/relay/CScheduleDisplay.vue'
+import { DateTime } from 'luxon'
+import { TimeUnit } from 'src/types/relay-config.interface'
+
+function secondsToTimeUnit(timeAsSeconds: number): TimeUnit {
+  const hour = Math.floor(timeAsSeconds / 3600)
+  const reducedSeconds = timeAsSeconds - hour * 3600
+  const minute = Math.floor(reducedSeconds / 60)
+  const second = reducedSeconds - minute * 60
+
+  return {
+    hour,
+    minute,
+    second,
+  }
+}
 
 export default defineComponent({
   components: {
@@ -48,16 +80,34 @@ export default defineComponent({
 
   emits: ['update:modelValue'],
 
-  setup() {
+  setup(props) {
     const width = ref(0)
     function onResize(value: { width: number }) {
       width.value = value.width
     }
 
+    const forPresentation = computed(() => {
+      return props.modelValue.map((entry) => {
+        return {
+          entry,
+          i18nParts: {
+            start: DateTime.now()
+              .set(secondsToTimeUnit(entry.start))
+              .toLocaleString(DateTime.TIME_WITH_SECONDS),
+            end: DateTime.now()
+              .set(secondsToTimeUnit(entry.end))
+              .toLocaleString(DateTime.TIME_WITH_SECONDS),
+            state: entry.state.toLowerCase(),
+          },
+        }
+      })
+    })
+
     return {
       width,
       onResize,
       SCHEDULE_BAR_HEIGHT: 10,
+      forPresentation,
     }
   },
 })
