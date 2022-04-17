856f5a03ddfe8a3b1b56269aca225eea03dd52a4
Felix <jonfelixrico@gmail.com>
2022-04-18 00:43:46 +0800

    Create utils and store for setSchedule

---
+++ src/stores/set-schedule-store.ts
@@ -0,0 +1,54 @@
+import { defineStore } from 'pinia'
+import { convertSingleOnScheduleToScheduleArray } from 'src/components/relay-details/set-schedule.utils'
+import { MAX_SECONDS } from 'src/components/relay/relay.constants'
+
+export interface SetScheduleStore {
+  singleOn: {
+    start: string | null
+    end: string | null
+  }
+
+  cycle: {
+    start: string | null
+    end: string | null
+  }
+}
+
+export const useCounterStore = defineStore('counter', {
+  state: (): SetScheduleStore => ({
+    singleOn: {
+      start: null,
+      end: null,
+    },
+
+    cycle: {
+      start: null,
+      end: null,
+    },
+  }),
+  getters: {
+    singleOnSchedule({ singleOn }) {
+      const { start, end } = singleOn
+
+      if (!start || !end) {
+        return [
+          {
+            start: 0,
+            end: MAX_SECONDS,
+            state: 'OFF',
+          },
+        ]
+      }
+
+      return convertSingleOnScheduleToScheduleArray(start, end)
+    },
+  },
+  actions: {
+    setSingleOnSchedule(start: string, end: string) {
+      this.singleOn = {
+        start,
+        end,
+      }
+    },
+  },
+})
