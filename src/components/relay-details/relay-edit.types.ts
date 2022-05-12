e57cc4ed58cddeefff0d7274597c595e120ca60c
Felix <jonfelixrico@gmail.com>
2022-05-13 00:18:06 +0800

    Remove unused code

--- src/components/relay-details/relay-edit.types.ts
+++
@@ -1,9 +0,0 @@
-import { PresentationScheduleEntry } from '../relay/relay-schedule-presentation.utils'
-
-export interface ScheduleEntryForEditing extends PresentationScheduleEntry {
-  /**
-   * Just an internal id for editing-related components. It helps us differentiate
-   * items better than just relying on indexes.
-   */
-  id: string
-}
